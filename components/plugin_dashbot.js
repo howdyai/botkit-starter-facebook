d4e33f9836c7dfdb07f0532ce868ff32287335e8
Dennis Yang <sinned@gmail.com>
2017-01-31 13:31:09 -0800

    Changed Dashbot API call to use Facebook instead of Slack

--- components/plugin_dashbot.js
+++ components/plugin_dashbot.js
@@ -3,7 +3,7 @@ module.exports = function(controller) {
     // Dashbot is a turnkey analytics platform for bots.
     // Sign up for a free key here: https://www.dashbot.io/ to see your bot analytics in real time.
     if (process.env.DASHBOT_API_KEY) {
-      var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).slack;
+      var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).facebook;
       controller.middleware.receive.use(dashbot.receive);
       controller.middleware.send.use(dashbot.send);
       controller.log.info('Thanks for using Dashbot. Visit https://www.dashbot.io/ to see your bot analytics in real time.');
