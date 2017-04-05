9ed774130f69c9f5136b0292a425c9496d9ddc25
Ben Brown <benbrown@tentacle.local>
2017-04-05 11:36:58 -0500

    Add support for .env file

--- bot.js
+++ bot.js
@@ -17,6 +17,9 @@ This is a sample Facebook bot built with Botkit.
 
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
 
 if (!process.env.page_token) {
     console.log('Error: Specify a Facebook page_token in environment.');
