ac527db5322e69c3ec8174ad94217ec9056fe272
Ben Brown <ben@xoxco.com>
2017-05-16 15:51:31 -0500

    Automatically capture the app id from Facebook at boot time

--- components/get_app_id.js
+++ components/get_app_id.js
@@ -1,9 +1,28 @@
+var request = require('request');
+var debug = require('debug')('botkit:get_app_id');
+
 module.exports = function(controller) {
 
+    debug('Getting Facebook App ID...');
+    request.post('https://graph.facebook.com/app/?access_token=' + controller.config.access_token,
+        function(err, res, body) {
+            if (err) {
+                debug('Could not get Facebook App ID! Check your page token.');
+                throw new Error(err);
+            } else {
+              var json = null;
+              try {
+                  json = JSON.parse(body);
+              } catch(err) {
+                  debug('Error parsing JSON response from Facebook');
+                  throw new Error(err);                
+              }
+              if (json.error) {
+                throw new Error(json.error.message);
+              } else {
+                controller.config.app = json; 
+              }
+            }
+        });
 
-
-
-
-
-
-}
+};
