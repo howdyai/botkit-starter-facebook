a92018a4fc67b7f0b7e9dd8d596d0c221c9609dc
Ben Brown <ben@xoxco.com>
2017-05-16 16:11:34 -0500

    Get both the app id and page id for this bot

--- components/get_app_id.js
+++ components/get_app_id.js
@@ -21,6 +21,31 @@ module.exports = function(controller) {
                 throw new Error(json.error.message);
               } else {
                 controller.config.app = json; 
+
+
+                debug('Getting Facebook Page ID...');
+                request.get('https://graph.facebook.com/me?access_token=' + controller.config.access_token,
+                    function(err, res, body) {
+                        if (err) {
+                            debug('Could not get Facebook Page ID! Check your page token.');
+                            throw new Error(err);
+                        } else {
+                          var json = null;
+                          try {
+                              json = JSON.parse(body);
+                          } catch(err) {
+                              debug('Error parsing JSON response from Facebook');
+                              throw new Error(err);                
+                          }
+                          if (json.error) {
+                            throw new Error(json.error.message);
+                          } else {
+                            controller.config.page = json; 
+                          }
+                        }
+                    });
+              
+              
               }
             }
         });
