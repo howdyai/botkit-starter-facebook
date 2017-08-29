fabe1c172b9e67376f39cd6303e9eae8557b4e88
Ben Brown <ben@xoxco.com>
2017-08-29 09:55:33 -0500

    Update API calls for persistent menu

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -9,20 +9,38 @@ module.exports = function(controller) {
     controller.api.thread_settings.get_started('sample_get_started_payload');
     controller.api.thread_settings.menu([
         {
-            "type":"postback",
-            "title":"Hello",
-            "payload":"hello"
-        },
-        {
-            "type":"postback",
-            "title":"Help",
-            "payload":"help"
-        },
-        {
-          "type":"web_url",
-          "title":"Botkit Docs",
-          "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
-        },
-    ]);
+            "locale": "default",
+            "composer_input_disabled": false,
+            "call_to_actions": [    
+                {
+                    "type":"postback",
+                    "title":"Hello",
+                    "payload":"hello"
+                },
+                {
+                    "type":"postback",
+                    "title":"Help",
+                    "payload":"help"
+                },
+                {
+                  "type":"nested",
+                  "title":"Botkit Docs",
+                  "call_to_actions": [
+                      {
+                        type: "web_url",
+                        "title": "Facebook Docs",
+                        "url":"https://github.com/howdyai/botkit/blob/master/docs/readme-facebook.md",
+                        "webview_height_ratio":"full",
+                      },
+                      {
+                        type: "web_url",
+                        "title": "Main Readme",
+                        "url":"https://github.com/howdyai/botkit/blob/master/readme.md",
+                        "webview_height_ratio":"full",
+                      }                    
 
+                  ]
+                }
+            ]
+        }]);
 }
