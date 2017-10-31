6a03fa19f3311bc595f28502814f59e9d8952c98
Ben Brown <ben@xoxco.com>
2017-10-31 12:20:36 -0500

    remove dashbot plugin

--- bot.js
+++ bot.js
@@ -61,9 +61,6 @@ require(__dirname + '/components/onboarding.js')(controller);
 // Load in some helpers that make running Botkit on Glitch.com better
 require(__dirname + '/components/plugin_glitch.js')(controller);
 
-// Enable Dashbot.io plugin
-require(__dirname + '/components/plugin_dashbot.js')(controller);
-
 var normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
   require("./skills/" + file)(controller);
